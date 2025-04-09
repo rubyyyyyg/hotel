5b1f69135d8c3325884382fa5f241de0834aa9c1
Ruby Guzman <rubyguzmansoto@gmail.com>
2025-04-09 16:11:37 -0600

    lots of changes

---
+++ js/data.js
@@ -0,0 +1,78 @@
+export const hotels = [
+      {
+        "image": "images/1hotel.webp",
+        "name": "The Ritz-Carlton",
+        "phone": "+1 123-456-7890",
+        "address": [
+          "123 Luxury Ave",
+          "New York, NY 10001"
+        ]
+      },
+      {
+        "image": "images/2hotel.webp",
+        "name": "Four Seasons Hotel",
+        "phone": "+1 234-567-8901",
+        "address": [
+          "200 Park Lane",
+          "London, W1K 7DN"
+        ]
+      },
+      {
+        "image": "images/3hotel.webp",
+        "name": "The St. Regis",
+        "phone": "+1 345-678-9012",
+        "address": [
+          "1000 5th Ave",
+          "New York, NY 10128"
+        ]
+      },
+      {
+        "image": "images/4hotel.webp",
+        "name": "Bulgari Hotel",
+        "phone": "+1 456-789-0123",
+        "address": [
+          "Bulgari Hotel, 17 Knightsbridge",
+          "London, SW1X 7QL"
+        ]
+      },
+      {
+        "image": "images/5hotel.webp",
+        "name": "The Plaza Hotel",
+        "phone": "+1 567-890-1234",
+        "address": [
+          "Fifth Ave at Central Park South",
+          "New York, NY 10019"
+        ]
+      },
+      {
+        "image": "images/6hotel.webp",
+        "name": "Mandarin Oriental",
+        "phone": "+1 678-901-2345",
+        "address": [
+          "80 Columbus Circle",
+          "New York, NY 10023"
+        ]
+      },
+      {
+        "image": "images/7hotel.webp",
+        "name": "The Langham",
+        "phone": "+1 789-012-3456",
+        "address": [
+          "The Langham, 1C Portland Place",
+          "London, W1B 1JA"
+        ]
+      },
+      {
+        "image": "images/8hotel.webp",
+        "name": "Shangri-La Hotel",
+        "phone": "+1 890-123-4567",
+        "address": [
+          "Shangri-La Hotel, 18 Martin Place",
+          "Sydney, NSW 2000"
+        ]
+      }
+        ]
+
+    
+  
+  
